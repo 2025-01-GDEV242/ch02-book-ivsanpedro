2656673bad843a3307290af9736ae4835e6545d5
ivsanpedro <ivsanpedro2@gmail.com>
2025-02-03 12:26:08 -0500

    Adding method headers

--- Book.java
+++ Book.java
@@ -3,8 +3,8 @@
  * This might form part of a larger application such
  * as a library system, for instance.
  *
- * @author (Insert your name here.)
- * @version (Insert today's date here.)
+ * @author (Ivana San Pedro)
+ * @version (2/3/25)
  */
 class Book
 {
@@ -14,44 +14,66 @@ class Book
     private int pages;
     private String refNumber;
     private int borrowed;
+    private boolean courseText;
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle, int numPages, int numBorrowed)
+    public Book(String bookAuthor, String bookTitle, int numPages, int numBorrowed, boolean text)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = numPages;
         refNumber = "";
         borrowed = numBorrowed;
+        courseText = text;
     }
 
+    /**
+     * Returns the name of the author
+     */
     public String getAuthor()
     {
         return author;
     }
     
+    /**
+     * Returns the title of the book
+     */
     public String getTitle()
     {
         return title;
     }
     
+    /**
+     * Returns the number of pages in the book
+     */
     public int getPages()
     {
         return pages;
     }
     
+    /**
+     * Prints the author's name
+     */
     public void printAuthor()
     {
         System.out.println(author);
     }
     
+    /**
+     * Prints the title of the book
+     */
     public void printTitle()
     {
         System.out.println(title);
     }
     
+    /**
+     * Prints the details of the book
+     * Includes the title, author's name, number of pages, the reference number, and the 
+     * number of times the book has been borrowed
+     */
     public void printDetails()
     {
         System.out.println("Title: " + title);
@@ -68,6 +90,10 @@ class Book
         System.out.println("Number of times it has been borrowed: " + borrowed);
     }
     
+    /**
+     * Sets the reference number for a library
+     * Prints out an error message if the String length is not at least three characters
+     */
     public void setRefNumber(String ref)
     {
         if (ref.length() >= 3)
@@ -81,19 +107,37 @@ class Book
         }
     }
     
+    /**
+     * Returns the reference number
+     */
     public String getrefNumber()
     {
         return refNumber;
     }
     
+    /**
+     * Each time it is called, the field updates by 1 each time
+     * Returns the number of times the book has been borrowed
+     */
     public int borrow()
     {
         borrowed+=1;
         return borrowed;
     }
     
+    /**
+     * Prints the number of times the book has been borrowed
+     */
     public int getBorrowed()
     {
         return borrowed;
+    }
+    
+    /**
+     * Returns if the text is used as a textbook on a course
+     */
+    public boolean isCourseText()
+    {
+        return courseText;
     }
 }
